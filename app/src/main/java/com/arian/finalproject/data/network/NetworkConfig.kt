2e1ffcf62a02af6d8c2181f8bedacb87ea13876e
ArianWicaksono09 <arian.wicaksono58@gmail.com>
2021-12-15 01:32:15 +0800

    Add API Config

---
+++ app/src/main/java/com/arian/finalproject/data/network/NetworkConfig.kt
@@ -0,0 +1,30 @@
+package com.arian.finalproject.data.network
+
+import com.arian.finalproject.BuildConfig
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class NetworkConfig {
+
+    private fun getNetwork(): Retrofit {
+
+        val loggingInterceptor =
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+
+    fun api(): ApiEndPoint {
+        return getNetwork().create(ApiEndPoint::class.java)
+    }
+}
